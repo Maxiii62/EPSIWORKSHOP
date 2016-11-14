4152de2c65ad4297e2bff93f1c6fe810b4fc816a
Maxiii62 <maxime.roussel1@epsi.fr>
2016-11-14 11:59:26 +0100

    Initialisation de l'environnement
    ajouts de quelques fichier

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+        <img src="materialize/img/UEFA_Euro_2016_logo.png" alt="Logo Euro 2016 - FRANCE">
+    </div>
+  </nav>
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center teal-text text-lighten-2">Beteclic</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">Le seul site au monde gratuit vous permettant de devenir le meilleur parieur au monde</h5>
+        </div>
+        <div class="row center">
+          <button id="cnxBtn" class="btn-large waves-effect waves-light teal lighten-1">Connexion</button>
+        </div>
+        <br><br>
+
+      </div>
+    </div>
+    <div class="parallax"><img src="materialize/img/messi.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+            <h5 class="center">Pariez dès maintenant</h5>
+
+            <p class="light">Inscrivez-vous dés à présent pour devenir le meilleur des parieurs sur l'Euro 2016.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">Amusante</h5>
+
+            <p class="light">Mesurez vous à vos amis et devenez le meilleur parmis vos amis !.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Pari</h5>
+
+            <p class="light">Notre site offre la possiblité de parier sur le score ainsi que de prédire quelle équipe emportera le match.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light">Remportez jusqu'a plus de 100 fois votre mise !</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="materialize/img/realmadrid.jpg" alt="Unsplashed background img 2"></div>
+  </div>
+
+  <div class="container">
+    <div class="section">
+
+      <div class="row">
+        <div class="col s12 center">
+          <h3><i class="mdi-content-send brown-text"></i></h3>
+          <h4>Contactez-nous</h4>
+          <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light">L'un des sites le meilleurs au monde pour les paris sportifs en ligne</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="materialize/img/dortmund.jpg" alt="Unsplashed background img 3"></div>
+  </div>
+
+
+
+  <div id="modalConnexion" class="modal">
+    <div class="row">
+     <div class="modal-content">
+      <h4>Connexion</h4><hr>
+        <div class="row">
+          <div class="input-field col s10">
+            <i class="material-icons prefix">account_circle</i>
+            <input id="login" type="text" class="validate">
+            <label for="icon_prefix">Login</label>
+          </div>
+          <div class="input-field col s10">
+            <i class="material-icons prefix">vpn_key</i>
+            <input id="password" type="password" class="validate">
+            <label for="icon_prefix">Password</label>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <a class="modal-action modal-close waves-effect waves-green btn-flat" id="sbmtCnx" >Connexion</a>
+        <a class="modal-action modal-close waves-effect waves-green btn-flat closeModal">Fermer</a>
+      </div>
+
+ </div>
+
+  </div>
+  </body>
+
+</html>
---
+++ ws/IWebServiciable.php
@@ -0,0 +1,7 @@
+<?php
+interface IWebServiciable {
+    function __construct();
+
+    public function doPost();
+}
+?>
