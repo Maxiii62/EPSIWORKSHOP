45713e4f8df1ab8a1e1ed4ccf1ae759b1050bb09
Maxiii62 <maxime.roussel1@epsi.fr>
2016-11-14 23:39:02 +0100

    API GOOGLE
    Différents types de locomotion

--- index.php
+++ index.php
@@ -8,13 +8,21 @@
   </head>
   <body>
     <div id="floating-panel">
+      <b>Mode de transport: </b>
+      <select id="mode">
+        <option value="DRIVING">Voiture</option>
+        <option value="WALKING">Marche</option>
+        <option value="BICYCLING">Vélo</option>
+        <option value="TRANSIT">Transport en commun</option>
+      </select>
+
+      <b>Rayon: </b>
+      <input type="text" id="rayon" placeholder="Indiquez un rayon">
     </div>
     <div id="right-panel"></div>
     <div id="map"></div>
 
   </body>
-
-
 
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgaxtdS0y_Mwk-ennUmyB9mHlpQbBGHLg&libraries=places&callback=initMap" async defer></script>
   <script src="front/js/google.js" />
