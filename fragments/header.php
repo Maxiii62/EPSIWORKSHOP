1d34723cf21fb2998933c1b3008e1c83a00458ce
acoussemaeker <arnaud.cousco@hotmail.fr>
2016-11-15 15:59:38 +0100

    Vue : nouveau Header

--- fragments/header.php
+++ fragments/header.php
@@ -1,4 +1,20 @@
-
-<?php
-echo 'test';
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="../../materialize/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+        <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+        <title><?php $title ?></title>
+    </head>
+    <body>
+  <nav>
+    <div class="nav-wrapper blue-grey">
+      <a href="#" class="brand-logo right blue-grey">EATinéraire</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down blue-grey">
+        <li><a href="">Carte</a></li>
+        <li><a href="">Historique</a></li>
+        <li><a href="">Mon profil</a></li>
+      </ul>
+    </div>
+  </nav>    
+        
