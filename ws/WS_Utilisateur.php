beccb83bbcade5baeacb89ce3b160d21f9a0e00b
Gomel <benjamin.gomel@epsi.fr>
2016-11-15 16:35:56 +0100

    ENCORE ET ENCORE

--- ws/WS_Utilisateur.php
+++ ws/WS_Utilisateur.php
@@ -4,6 +4,8 @@
     const UPDATE_USER = 'updateUser';
     const GET_USER = 'getUser';
     const GET_CLASSEMENT = 'getClassement';
+    const VERIF_USER = 'verifUser';
+
 
 class WS_Utilisateur implements IWebServiciable{
 
@@ -20,17 +22,20 @@ class WS_Utilisateur implements IWebServiciable{
 
         switch ($_POST['action']) {
             case ADD_USER :
-                $sql = "INSERT INTO Utilisateur ('nom', 'prenom', 'dateNaissance', 'mail', 'numeroTelephone', 'nombrePoints') VALUES (" .$_POST['nom']. ", ". $_POST['prenom'] . ", ". $_POST['dateNaissance'] . ", ". $_POST['numeroTelephone'] .", 0)";
+                $sql = "INSERT INTO Utilisateur ('nom', 'prenom', 'dateNaissance', 'mail', 'numeroTelephone', 'password', 'nombrePoints') VALUES (" .$_POST['nom']. ", ". $_POST['prenom'] . ", ". $_POST['dateNaissance'] . ", ". $_POST['numeroTelephone'] .", ". $_POST['password'] .",0)";
                 return  execReqWithoutResult($sql);
             case UPDATE_USER :
-                $sql = "UPDATE UTILISATEUR SET nom = " .$_POST['nom']. ", prenom = " .$_POST['nom']. ", dateNaissance = " .$_POST['dateNaissance']. ", mail = " .$_POST['mail']. ", numeroTelephone = " .$_POST['numeroTelephone'];
+                $sql = "UPDATE UTILISATEUR SET nom = " .$_POST['nom']. ", prenom = " .$_POST['nom']. ", dateNaissance = " .$_POST['dateNaissance']. ", mail = " .$_POST['mail']. ", numeroTelephone = " .$_POST['numeroTelephone']. ", password = " .$_POST['password'];
                 return execReqWithoutResult($sql);
             case GET_USER :
-                $sql = "SELECT * FROM UTILISATEUR WHERE id = " . $_POST['idRdv'];
+                $sql = "SELECT * FROM UTILISATEUR WHERE id = " . $_POST['idUtilisateur'];
                 return returnOneLine($sql);
             case GET_CLASSEMENT :
                 $sql = "SELECT * FROM Utilisateur ORDER BY nombrePoints";
                 return returnOneArray($sql);
+            case VERIF_USER :
+                $sql = "SELECT * FROM UTILISATEUR WHERE mail = " . $_POST['mail']. " AND password = " .$_POST['password'];
+                return returnOneLine($sql);
             default:
                 Helper::ThrowAccessDenied();
                 break;
