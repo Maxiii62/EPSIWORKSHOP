9252de478f8f7eb1d8fea3866dfd2176f8772f64
Max62 <maxime-lecaille@hotmail.fr>
2016-11-15 13:27:51 +0100

    modification script sql

--- SQL/ScriptBDD.sql
+++ SQL/ScriptBDD.sql
@@ -5,7 +5,6 @@
 CREATE SCHEMA IF NOT EXISTS `workshopEATineraire` DEFAULT CHARACTER SET utf8 ;
 USE `workshopEATineraire` ;
 
-
 #------------------------------------------------------------
 # Table: Utilisateur
 #------------------------------------------------------------
@@ -16,7 +15,6 @@ CREATE TABLE Utilisateur(
         prenom          Varchar (25) NOT NULL ,
         dateNaissance   Date NOT NULL ,
         mail            Varchar (25) NOT NULL ,
-		password		Varchar(25) NOT NULL,
         numeroTelephone Varchar (25) NOT NULL ,
         nombrePoints    Int ,
         PRIMARY KEY (id )
@@ -28,11 +26,11 @@ CREATE TABLE Utilisateur(
 #------------------------------------------------------------
 
 CREATE TABLE Rdv(
-        id         int (11) Auto_increment  NOT NULL ,
-        horaire    Varchar (25) NOT NULL ,
-        idCreateur Int NOT NULL ,
-        dateRdv    Date NOT NULL ,
-        id_Lieu    Int NOT NULL ,
+        id            int (11) Auto_increment  NOT NULL ,
+        horaire       Varchar (25) NOT NULL ,
+        idUtilisateur Int NOT NULL ,
+        dateRdv       Date NOT NULL ,
+        id_Lieu       Int NOT NULL ,
         PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
@@ -76,4 +74,3 @@ ALTER TABLE Rdv ADD CONSTRAINT FK_Rdv_id_Lieu FOREIGN KEY (id_Lieu) REFERENCES L
 ALTER TABLE Verdict ADD CONSTRAINT FK_Verdict_id_Rdv FOREIGN KEY (id_Rdv) REFERENCES Rdv(id);
 ALTER TABLE utilisateur_rdv ADD CONSTRAINT FK_utilisateur_rdv_id FOREIGN KEY (id) REFERENCES Utilisateur(id);
 ALTER TABLE utilisateur_rdv ADD CONSTRAINT FK_utilisateur_rdv_id_Rdv FOREIGN KEY (id_Rdv) REFERENCES Rdv(id);
-
