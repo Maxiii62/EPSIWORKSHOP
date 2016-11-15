d5d07690cc58ecb245b856e23760020ba7159ea0
Gomel <benjamin.gomel@epsi.fr>
2016-11-15 14:11:02 +0100

    WS Rdv

---
+++ ws/WS_Rdv.php
@@ -0,0 +1,40 @@
+<?php
+
+const GET_MINE = "getMine";
+const GET_BYID = "getByid";
+const GET_SEARCH = "getSearch";
+const GET_DETAILS = "getDetails";
+
+class WS_Rdv implements IWebServiciable
+{
+
+    function __construct()
+    {
+
+    }
+
+    public function doPost()
+    {
+
+        if (!isset($_POST['action']))
+            Helper::ThrowAccessDenied();
+
+        switch ($_POST['action']) {
+            case GET_MINE :
+                $sql = "SELECT rdv.horraire, rdv.dateRdv, lieu.nomLieu FROM Rdv rdv INNER JOIN utilisateur_rdv urdv ON urdv.idRdv = rdv.id AND Lieu lieu ON lieu.id = rdv.idLieu  WHERE urdv.idUser =" . $_POST['idUser'];
+                return returnOneArray($sql);
+            case GET_BYID :
+                $sql = "SELECT rdv.horraire, rdv.dateRdv, lieu.nomLieu FROM Rdv rdv INNER JOIN utilisateur_rdv urdv ON urdv.idRdv = rdv.id AND Lieu lieu ON lieu.id = rdv.idLieu  WHERE urdv.idRdv =" . $_POST['idRdv'];
+                return returnOneLine($sql);
+            case GET_SEARCH :
+                $sql = "SELECT rdv.horraire, rdv.dateRdv, lieu.nomLieu FROM Rdv rdv INNER JOIN utilisateur_rdv urdv ON urdv.idRdv = rdv.id AND Lieu lieu ON lieu.id = rdv.idLieu  WHERE lieu.nom = " . $_POST['idLieu'] . " AND rdv.horraire = ". $_POST['horraire'] . " AND rdv.date = ". $_POST['date'];
+                return returnOneArray($sql);
+            case GET_DETAILS :
+                $sql = "SELECT * FROM Rdv rdv INNER JOIN Utilisateur user ON user.id = rdv.idCreateur LEFT JOIN Verdict verd ON verd.idRdv = rdv.id WHERE rdv.id ="  . $_POST['idRdv'];
+                return returnOneArray($sql);
+            default:
+                Helper::ThrowAccessDenied();
+                break;
+        }
+    }
+}
