7e36573eabd9be34e2b96ef0da9f17ab89eb1d9e
acoussemaeker <arnaud.cousco@hotmail.fr>
2016-11-16 14:49:00 +0100

    Vue : page historique

---
+++ front/html/Historical.php
@@ -0,0 +1,35 @@
+
+<?php
+$title = "Historique";
+include '../../fragments/header.php';
+?>
+<body>
+    <div class="contain row card horizontal">
+        </br>
+        <h3 class="center">Historique</h3>
+        <table class="">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Conducteur</th>
+                    <th>Lieu</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr onclick="alert('la vous pourrais noter le rendez vous');"> 
+                    <td>18/11/2016</td>
+                    <td>Roussel Maxime</td>
+                    <td>Macdonalds</td>
+                </tr>
+            </tbody>
+             
+        </table>
+ </br>
+    </div>
+
+   
+    <?php
+    include '../../fragments/footer.php';
+    ?>
+</body>
+</html>
