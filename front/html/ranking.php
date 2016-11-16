8dd009833ebe307db2abb660bb791be687b7c921
acoussemaeker <arnaud.cousco@hotmail.fr>
2016-11-16 20:51:16 +0100

    truc mais pas fini pour les classement

---
+++ front/html/ranking.php
@@ -0,0 +1,36 @@
+<?php
+
+$title = "Prendre Rendez vous";
+include '../../fragments/header.php';
+?>
+<div id="login-page">
+    <h3>Classement</h3>
+<table id="tab_topyoovi" class="trietable dataTable card ">
+    <thead>
+        <tr>
+            <th>Position</th>
+            <th>Nom</th>
+        </tr>
+    </thead>
+
+    <tbody role="alert" aria-live="polite" aria-relevant="all">
+        <tr class="odd">
+            <td>1er</td>
+            <td class="td-mc1 ">moi ! </td>
+        </tr><tr class="even">
+            <td>1er</td>
+            <td class="td-mc1 ">moi ! </td>
+        </tr><tr class="odd">
+            <td>1er</td>
+            <td class="td-mc1 ">moi ! </td>
+        </tr><tr class="even">
+            <td>1er</td>
+            <td class="td-mc1 ">moi ! </td>
+    </tbody>
+</table>
+</div>
+
+<?php
+
+include '../../fragments/footer.php';
+?>
