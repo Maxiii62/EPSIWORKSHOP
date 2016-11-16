584fecb1316ad3d4b8674c2435a73a8a45f88741
Gomel <benjamin.gomel@epsi.fr>
2016-11-16 09:41:50 +0100

    Askip je dois commit

--- fragments/header.php
+++ fragments/header.php
@@ -7,18 +7,15 @@
         <title><?php echo $title; ?></title>
     </head>
     <body>
-        <?php if($title != "EATinéraire"){
-            
-        ?>
   <nav>
     <div class="nav-wrapper blue-grey ">
       <ul id="nav-mobile" class="hide-on-med-and-down blue-grey">
           <div class="left">
             <li><a href="">Carte</a></li>
              <li><a href="">Historique</a></li>
-             <li><a href="">Mon profil</a></li>
           </div>
           <div class="right">
+              <li><a href="">Mon profil</a></li>
               <li><a class="" href="../html/Inscription.php">Inscription</a></li>
               <li><a class="" href="../html/Login.php">Connexion</a></li>
               <li><a class="" href="../html/Login.php">Déconnexion</a></li>
@@ -26,5 +23,3 @@
       </ul>
     </div>
   </nav>
-        <?php } 
-        ?>
