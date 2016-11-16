945583e6fa32150235a3d0226cebede46c12d9da
acoussemaeker <arnaud.cousco@hotmail.fr>
2016-11-16 11:34:27 +0100

    vue : retour inscription et footer

--- front/html/Inscription.php
+++ front/html/Inscription.php
@@ -1,60 +1,61 @@
-<?php$title = "Inscription";?>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<link href="../../materialize/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-		<link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-		<title>Inscription</title>
-	</head>
-	<body>
-		<div id="login-page" class="row">
-			<div class="transparence">
-				<div id="Register_Container" class="row card horizontal">
-					<form class="col s12">
-						<div class="row">
-							<div class="input-field col s6">
-								<input id="first_name" type="text" class="validate">
-								<label for="first_name"><i class="tiny material-icons">mode_edit</i>Prénom</label>
-							</div>
-							<div class="input-field col s6">
-								<input id="last_name" type="text" class="validate">
-								<label for="last_name"><i class="tiny material-icons">mode_edit</i>Nom</label>
-							</div>
-						</div>
-						<div class="row">
-							<div class="input-field col s12">
-								<input id="password" type="password" class="validate">
-								<label for="password"><i class="tiny material-icons">lock</i>Mot de passe</label>
-							</div>
-						</div>
-						<div class="row">
-							<div class="input-field col s12">
-								<input id="email" type="email" class="validate">
-								<label for="email"><i class="tiny material-icons">email</i>Email</label>
-							</div>
-						</div>
-						<div class="row">
-							<div class="input-field col s12">
-								<input id="telephone" type="email" class="validate">
-								<label for="telephone"><i class="tiny material-icons">phone</i>Telephone</label>
-							</div>
-						</div>
-						<div class="row">
-							<div class="input-field col s12">
-								<input id="datepicker_register" type="date" class="validate" placeholder="Date de naissance">
-							</div>
-						</div>
-						<span><button class="btn waves-effect waves-light" type="button" id="valider" name="action">Valider</button></span>
-						<button class="btn waves-effect waves-light" type="button reset" value="Reset!" id="reset" name="action">Effacer</button>
-					</span>
-					<a href="/epsiworkshop/front/html/Login.php"><button class="btn waves-effect waves-light" type="button" id="back" name="action">Retour</button></a>
-					<br>
-					<p></p>
-				</form>
-			</div>
-		</div>
-	</div>
+<?php
+$title = "Inscription";
+include '../../fragments/header.php';
+?>
+
+<head>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+</head>
+<body>
+    <div id="login-page" class="row">
+        <div class="transparence">
+            <div id="Register_Container" class="row card horizontal">
+                <form class="col s12">
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input id="first_name" type="text" class="validate">
+                            <label for="first_name"><i class="tiny material-icons">mode_edit</i>Prénom</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input id="last_name" type="text" class="validate">
+                            <label for="last_name"><i class="tiny material-icons">mode_edit</i>Nom</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="password" type="password" class="validate">
+                            <label for="password"><i class="tiny material-icons">lock</i>Mot de passe</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="email" type="email" class="validate">
+                            <label for="email"><i class="tiny material-icons">email</i>Email</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="telephone" type="email" class="validate">
+                            <label for="telephone"><i class="tiny material-icons">phone</i>Telephone</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="datepicker_register" type="date" class="validate" placeholder="Date de naissance">
+                        </div>
+                    </div>
+                    <span><button class="btn waves-effect waves-light" type="button" id="valider" name="action">Valider</button></span>
+                    <button class="btn waves-effect waves-light" type="button reset" value="Reset!" id="reset" name="action">Effacer</button>
+                    </span>
+                    <a href="/epsiworkshop/front/html/Login.php"><button class="btn waves-effect waves-light" type="button" id="back" name="action">Retour</button></a>
+                    <br>
+                    <p></p>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
-<?php include '../../fragments/footer.php';?>
+<?php include '../../fragments/footer.php'; ?>
--- front/html/Mon_Profil.php
+++ front/html/Mon_Profil.php
@@ -7,21 +7,31 @@ include '../../fragments/header.php';
     <div class="contain row card horizontal">
         </br>
         <h3 class="center">Mon Profil</h3>
-        <table>
+        <table class="responsive-table">
 
             <tbody>
-                <tr>
-                    <td>Alvin</td>
+                <tr> 
+                    <td><h5>Nom</h5></td>
                     <td>Eclair</td>
                     <td>$0.87</td>
                 </tr>
                 <tr>
-                    <td>Alan</td>
+                   <td><h5>Prénom</h5></td>
                     <td>Jellybean</td>
                     <td>$3.76</td>
                 </tr>
                 <tr>
-                    <td>Jonathan</td>
+                    <td><h5>Email</h5></td>
+                    <td>Lollipop</td>
+                    <td>$7.00</td>
+                </tr>
+                <tr>
+                    <td><h5>Date de naisance</h5></td>
+                    <td>Lollipop</td>
+                    <td>$7.00</td>
+                </tr>
+                <tr>
+                    <td><h5>N° de telephone</h5></td>
                     <td>Lollipop</td>
                     <td>$7.00</td>
                 </tr>
