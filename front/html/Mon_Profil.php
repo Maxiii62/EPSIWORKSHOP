28a70b89f204e77b4b0c21bbb2eb9ee6368cf78b
acoussemaeker <arnaud.cousco@hotmail.fr>
2016-11-17 09:36:07 +0100

    mon profil footer

--- front/html/Mon_Profil.php
+++ front/html/Mon_Profil.php
@@ -1,88 +1,88 @@
 <body>
-<?php
-$title = "Mon profil";
-include '../../fragments/header.php';
+    <?php
+    $title = "Mon profil";
+    include '../../fragments/header.php';
 
-    if (!isset($_SESSION['monUserCo'])){
+    if (!isset($_SESSION['monUserCo'])) {
         header('Location: ../html/Login.php');
     }
+    ?>
+    <main>
+        <div id="login-page" class="row">
+            <div class="transparence col s12 z-depth-4 card-panel">
+                <form class="login-form">
+                    <div class="row">
+                        <div class="input-field col s12 center">
 
-?>
-    <div id="login-page" class="row">
-        <div class="transparence col s12 z-depth-4 card-panel">
-            <form class="login-form">
-                <div class="row">
-                    <div class="input-field col s12 center">
-
-                        <p class="center login-form-text">Mon Profil</p>
+                            <p class="center login-form-text">Mon Profil</p>
+                        </div>
                     </div>
-                </div>
 
-                <ul id="profile-page-about-details" class="collection z-depth-1">
-                    <li class="collection-item">
-                        <div class="row">
-                            <div class="col s5 grey-text darken-1"><i class="mdi-action-wallet-travel"></i> Nom</div>
-                            <div class="col s7 grey-text text-darken-4 right-align">
-                            <?php
-                                echo $_SESSION['monUserCo']['nom'];
-                            ?>
+                    <ul id="profile-page-about-details" class="collection z-depth-1">
+                        <li class="collection-item">
+                            <div class="row">
+                                <div class="col s5 grey-text darken-1"><i class="mdi-action-wallet-travel"></i> Nom</div>
+                                <div class="col s7 grey-text text-darken-4 right-align">
+                                    <?php
+                                    echo $_SESSION['monUserCo']['nom'];
+                                    ?>
+                                </div>
                             </div>
-                        </div>
-                    </li>
-                    <li class="collection-item">
-                        <div class="row">
-                            <div class="col s5 grey-text darken-1"><i class="mdi-social-poll"></i>Pr&eacutenom</div>
-                            <div class="col s7 grey-text text-darken-4 right-align">
-                            <?php
-                                echo $_SESSION['monUserCo']['prenom'];
-                            ?>
+                        </li>
+                        <li class="collection-item">
+                            <div class="row">
+                                <div class="col s5 grey-text darken-1"><i class="mdi-social-poll"></i>Pr&eacutenom</div>
+                                <div class="col s7 grey-text text-darken-4 right-align">
+                                    <?php
+                                    echo $_SESSION['monUserCo']['prenom'];
+                                    ?>
+                                </div>
                             </div>
-                        </div>
-                    </li>
-                    <li class="collection-item">
-                        <div class="row">
-                            <div class="col s5 grey-text darken-1"><i class="mdi-social-domain"></i>Date de naissance</div>
-                            <div class="col s7 grey-text text-darken-4 right-align"> <?php
-                                echo $_SESSION['monUserCo']['dateNaissance'];
-                                ?></div>
-                        </div>
-                    </li>
-                    <li class="collection-item">
-                        <div class="row">
-                            <div class="col s5 grey-text darken-1"><i class="mdi-social-cake"></i>Mail</div>
-                            <div class="col s7 grey-text text-darken-4 right-align"> <?php
-                                echo $_SESSION['monUserCo']['mail'];
-                                ?></div>
-                        </div>
-                    </li>
-                    <li class="collection-item">
-                        <div class="row">
-                            <div class="col s5 grey-text darken-1"><i class="mdi-social-cake"></i>T&eacutel&eacutephone</div>
-                            <div class="col s7 grey-text text-darken-4 right-align"> <?php
-                            echo $_SESSION['monUserCo']['numeroTelephone'];
-                            ?></div>
-                        </div>
-                    </li>
-                    <li class="collection-item">
-                        <div class="row">
-                            <div class="col s5 grey-text darken-1"><i class="mdi-social-cake"></i>Nombre de points</div>
-                            <div class="col s7 grey-text text-darken-4 right-align"> <?php
-                                echo $_SESSION['monUserCo']['nombrePoints'];
-                                ?></div>
-                        </div>
-                    </li>
-                </ul>
+                        </li>
+                        <li class="collection-item">
+                            <div class="row">
+                                <div class="col s5 grey-text darken-1"><i class="mdi-social-domain"></i>Date de naissance</div>
+                                <div class="col s7 grey-text text-darken-4 right-align"> <?php
+                                    echo $_SESSION['monUserCo']['dateNaissance'];
+                                    ?></div>
+                            </div>
+                        </li>
+                        <li class="collection-item">
+                            <div class="row">
+                                <div class="col s5 grey-text darken-1"><i class="mdi-social-cake"></i>Mail</div>
+                                <div class="col s7 grey-text text-darken-4 right-align"> <?php
+                                    echo $_SESSION['monUserCo']['mail'];
+                                    ?></div>
+                            </div>
+                        </li>
+                        <li class="collection-item">
+                            <div class="row">
+                                <div class="col s5 grey-text darken-1"><i class="mdi-social-cake"></i>T&eacutel&eacutephone</div>
+                                <div class="col s7 grey-text text-darken-4 right-align"> <?php
+                                    echo $_SESSION['monUserCo']['numeroTelephone'];
+                                    ?></div>
+                            </div>
+                        </li>
+                        <li class="collection-item">
+                            <div class="row">
+                                <div class="col s5 grey-text darken-1"><i class="mdi-social-cake"></i>Nombre de points</div>
+                                <div class="col s7 grey-text text-darken-4 right-align"> <?php
+                                    echo $_SESSION['monUserCo']['nombrePoints'];
+                                    ?></div>
+                            </div>
+                        </li>
+                    </ul>
 
-                <div class="row">
-                    <div class="input-field col s12" >
-                        <a id="modifierProfil" class="btn waves-effect waves-light col s12">Modifier</a>
+                    <div class="row">
+                        <div class="input-field col s12" >
+                            <a id="modifierProfil" class="btn waves-effect waves-light col s12">Modifier</a>
+                        </div>
                     </div>
-                </div>
 
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
-
+    </main>
     <?php
     include '../../fragments/footer.php';
     ?>
