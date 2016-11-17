63b7b90a02163c207f1b4c273de6a1d1d9dbb959
acoussemaeker <arnaud.cousco@hotmail.fr>
2016-11-17 09:29:01 +0100

    Vue : correction du footer sur la page historique

--- front/html/Historical.php
+++ front/html/Historical.php
@@ -1,33 +1,33 @@
-
-<?php
-$title = "Historique";
-include '../../fragments/header.php';
-?>
 <body>
-    <div class="contain row card horizontal">
-        </br>
-        <h3 class="center">Historique</h3>
-        <table class="">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Conducteur</th>
-                    <th>Lieu</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr onclick="alert('la vous pourrais noter le rendez vous');"> 
-                    <td>18/11/2016</td>
-                    <td>Roussel Maxime</td>
-                    <td>Macdonalds</td>
-                </tr>
-            </tbody>
-             
-        </table>
- </br>
-    </div>
+    <?php
+    $title = "Historique";
+    include '../../fragments/header.php';
+    ?>
+    <main>
+        <div class="contain row card horizontal">
+            </br>
+            <h3 class="center">Historique</h3>
+            <table class="">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Conducteur</th>
+                        <th>Lieu</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr onclick="alert('la vous pourrais noter le rendez vous');"> 
+                        <td>18/11/2016</td>
+                        <td>Roussel Maxime</td>
+                        <td>Macdonalds</td>
+                    </tr>
+                </tbody>
 
-   
+            </table>
+            </br>
+        </div>
+
+    </main>
     <?php
     include '../../fragments/footer.php';
     ?>
