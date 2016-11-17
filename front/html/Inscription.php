2aaa3598c6cef9832671aa303f6f2453b7d441c1
acoussemaeker <arnaud.cousco@hotmail.fr>
2016-11-17 09:17:17 +0100

    Vue : correction du footer

--- front/html/Inscription.php
+++ front/html/Inscription.php
@@ -1,55 +1,63 @@
+</body>
 <?php $title = "Inscription";
-include '../../fragments/header.php';?>
+include '../../fragments/header.php';
+?>
+    <header>…</header>
+    <main class="Site-content">…</main>
 
-	<body>
-		<div id="login-page" class="row">
-			<div class="transparence">
-				<div id="Register_Container" class="row card horizontal">
-					<form class="col s12">
-						<div class="row">
-							<div class="input-field col s6">
-								<input id="first_name" type="text" class="validate reset">
-								<label for="first_name"><i class="tiny material-icons">mode_edit</i> Prénom</label>
-							</div>
-							<div class="input-field col s6">
-								<input id="last_name" type="text" class="validate reset">
-								<label for="last_name"><i class="tiny material-icons">mode_edit</i> Nom</label>
-							</div>
-						</div>
-						<div class="row">
-							<div class="input-field col s12">
-								<input id="password" type="password" class="validate reset">
-								<label for="password"><i class="tiny material-icons">lock</i> Mot de passe</label>
-							</div>
-						</div>
-						<div class="row">
-							<div class="input-field col s12">
-								<input id="email" type="email" class="validate reset">
-								<label for="email"><i class="tiny material-icons">email</i> Email</label>
-							</div>
-						</div>
-						<div class="row">
-							<div class="input-field col s12">
-								<input id="telephone" type="text" class="validate reset">
-								<label for="telephone"><i class="tiny material-icons">phone</i> Téléphone</label>
-							</div>
-						</div>
-						<div class="row">
-							<div class="input-field col s12">
-								<input id="datepicker_register" type="date" class="validate reset" placeholder="Date de naissance">
-							</div>
-						</div>
-						<span><button class="btn waves-effect waves-light" type="button" id="valider" name="action">Valider</button></span>
+   
 
-						<button class="btn waves-effect waves-light" type="button reset" value="Reset!" id="reset" name="action">Effacer</button>
-					</span>
-					<a href="/epsiworkshop/front/html/Appointement.php"><button class="btn waves-effect waves-light" type="button" id="back" name="action">Retour</button></a>
-					<br>
-					<p></p>
-				</form>
-			</div>
-		</div>
-	</div>
+ <main class="Site-content">
+<div id="login-page" class="row">
+    <div class="transparence">
+        <div id="Register_Container" class="row card horizontal">
+            <form class="col s12">
+                <div class="row">
+                    <div class="input-field col s6">
+                        <input id="first_name" type="text" class="validate reset">
+                        <label for="first_name"><i class="tiny material-icons">mode_edit</i> Prénom</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <input id="last_name" type="text" class="validate reset">
+                        <label for="last_name"><i class="tiny material-icons">mode_edit</i> Nom</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="password" type="password" class="validate reset">
+                        <label for="password"><i class="tiny material-icons">lock</i> Mot de passe</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="email" type="email" class="validate reset">
+                        <label for="email"><i class="tiny material-icons">email</i> Email</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="telephone" type="text" class="validate reset">
+                        <label for="telephone"><i class="tiny material-icons">phone</i> Téléphone</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="datepicker_register" type="date" class="validate reset" placeholder="Date de naissance">
+                    </div>
+                </div>
+                <span><button class="btn waves-effect waves-light" type="button" id="valider" name="action">Valider</button></span>
+
+                <button class="btn waves-effect waves-light" type="button reset" value="Reset!" id="reset" name="action">Effacer</button>
+                </span>
+                <a href="/epsiworkshop/front/html/Appointement.php"><button class="btn waves-effect waves-light" type="button" id="back" name="action">Retour</button></a>
+                <br>
+                <p></p>
+            </form>
+        </div>
+    </div>
+</div>
+</main>
+
+<?php include '../../fragments/footer.php'; ?>
 </body>
 </html>
-<?php include '../../fragments/footer.php';?>
