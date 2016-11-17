893e464bf0e59cf69fa870d65df821abba968ecf
acoussemaeker <arnaud.cousco@hotmail.fr>
2016-11-17 15:54:06 +0100

    bouton detail dans l'historique

--- front/html/Historical.php
+++ front/html/Historical.php
@@ -13,13 +13,16 @@
                         <th>Date</th>
                         <th>Conducteur</th>
                         <th>Lieu</th>
+                        <th>Détail</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr onclick="alert('la vous pourrais noter le rendez vous');"> 
                         <td>18/11/2016</td>
                         <td>Roussel Maxime</td>
                         <td>Macdonalds</td>
+                        <td>
+                            <a class="btn-floating btn-large waves-effect waves-light blue-grey"><i class="material-icons">add</i></a>
+                        </td>
                     </tr>
                 </tbody>
 
